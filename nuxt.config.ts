81e77f0678234fb032ac765cb798ab61c3db48cf
Васильев Александр <wyndace.av@ya.ru>
2023-05-10 05:40:12 +0500

    feat: Заменён TailwindCSS на Bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["nuxt-seo-kit"],
-  modules: ["@nuxtjs/tailwindcss", "nuxt-purgecss", "nuxt-svgo", "@nuxt/image-edge"],
+  app: {
+    head: {
+      meta: [
+        { name: "author", content: "Alexander Vasilev with StartBootstrap" }
+      ],
+    },
+  },
   runtimeConfig: {
     trailingSlash: true,
     indexable: false,
     public: {
       siteUrl: "http:/localhost:3000",
-      siteName: "Nuxt Site",
-      siteDescription: "Site base on Nuxt.",
+      siteName: "Resume - Start Bootstrap Theme",
+      siteDescription:
+        "Clarence Taylor's resume site. I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.",
       language: "en-US",
       titleSeparator: "|",
       trailingSlash: false,
---
+++ plugins/bootstrap.client.ts
@@ -0,0 +1,10 @@
+import { Modal } from "bootstrap";
+
+export default defineNuxtPlugin(() => ({
+  provide: {
+    bootstrap: {
+      Modal,
+    },
+  },
+}));
+
